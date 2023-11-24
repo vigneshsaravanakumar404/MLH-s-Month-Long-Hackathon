6571f6e79028f47177f6bb34dcc1662062e2574b
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-11-24 17:38:39 -0500

    Creation of indivudial files for each page
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

---
+++ Website/learn.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app(): 
+    st.header("Learn")
+    st.write("Educational resources to learn about recycling.")
---
+++ Website/scan.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.header("Scan")
+    st.write("Scan items to recycle them correctly.")
