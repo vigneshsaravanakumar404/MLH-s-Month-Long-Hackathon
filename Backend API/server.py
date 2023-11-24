fc9782478751d30aa248d6c67696578510c97e6b
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-11-24 14:52:49 -0500

    Working image detection model
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- Backend API/server.py
+++ Backend API/server.py
@@ -17,4 +17,30 @@ def draw_label(image, box, class_name, confidence):
 
 @app.route('/objects', methods=['POST'])
 def get_objects():
-    pass
+    data = request.json
+    if 'image' not in data:
+        return jsonify({"error": "No image provided"}), 400
+
+    image_data = data['image']
+    try:
+        objects = []
+        image_binary = base64.b64decode(image_data)
+        image = np.frombuffer(image_binary, dtype=np.uint8)
+        source = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        results = model(source)
+        result = results[0]
+        for box in result.boxes:
+            class_id = result.names[box.cls[0].item()]
+            cords = box.xyxy[0].tolist()
+            conf = round(box.conf[0].item(), 2)
+
+            draw_label(source, cords, class_id, conf)
+            objects.append({"class_id": class_id, "confidence": conf, "coordinates": cords})
+
+        _, buffer = cv2.imencode('.jpg', source)
+        encoded_image = base64.b64encode(buffer).decode('utf-8')
+        
+        return jsonify({"objects": objects, "image": encoded_image}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Invalid image"}), 400
