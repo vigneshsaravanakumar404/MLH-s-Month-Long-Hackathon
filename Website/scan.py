aa8923422fb8a7940328b3a3991d471565393cbd
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-11-24 18:36:08 -0500

    Progress Bar Incorperation
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- Website/scan.py
+++ Website/scan.py
@@ -2,18 +2,15 @@ import streamlit as st
 import requests
 import base64
 import json
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2
+import time
 
 # Functions
-
 def send_image_to_server(image, url):
     """
-    Sends an image to a server using a POST request.
+    Sends an image to a server using a POST request with retry on failure.
 
     Args:
-        image_path (str): The path to the image file.
+        image (bytes): The image data in bytes.
         url (str): The URL of the server to send the image to.
 
     Returns:
@@ -23,8 +20,15 @@ def send_image_to_server(image, url):
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     data = json.dumps({"image": encoded_string})
 
-    response = requests.post(url, data=data, headers=headers)
-    return response
+    for attempt in range(3):  # Retry up to 3 times
+        try:
+            response = requests.post(url, data=data, headers=headers)
+            return response
+        except requests.ConnectionError:
+            if attempt < 2:  # Don't show a message on the final attempt
+                st.warning("Connection error, retrying...")
+            time.sleep(1)  # Wait a bit before retrying
+    return None  # Return None if all retries fail
 
 def app():
     st.header("Scan")
@@ -33,11 +37,24 @@ def app():
     img_file_buffer = st.camera_input("Take a picture")
 
     if img_file_buffer is not None:
-        # Read the image into bytes
         image_bytes = img_file_buffer.getvalue()
-        response = send_image_to_server(image=image_bytes, url='http://localhost:5000/objects')
-        response_data = response.json()
-        st.write(response_data)
+
+        # Button to send the request
+        if st.button("Scan"):
+            with st.spinner("Processing..."):
+                progress_bar = st.progress(0)
+                for percent_complete in range(100):
+                    time.sleep(0.04)  # Simulate time passing (4 seconds total)
+                    progress_bar.progress(percent_complete + 1)
+            
+            response = send_image_to_server(image=image_bytes, url='http://localhost:5000/objects')
+            
+            if response is not None:
+                response_data = response.json()
+                st.write(response_data)
+                progress_bar.empty()
+            else:
+                st.error("Failed to connect to the server after several attempts.")
+                progress_bar.empty()
     else:
         st.write("No image selected")
-    
