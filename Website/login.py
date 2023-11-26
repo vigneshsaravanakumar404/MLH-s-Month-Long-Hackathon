cbf01b2999d0ebd845ac6935fc31989ecc9fc0b7
AKherb <81490748+AKherb@users.noreply.github.com>
2023-11-26 13:30:41 -0500

    Update login.py

--- Website/login.py
+++ Website/login.py
@@ -1,23 +1,52 @@
 import streamlit as st
-
-def check_credentials(username, password):
-    # Placeholder for actual authentication logic
-    # Return True if credentials are valid, False otherwise
-    return True if username == "1" and password == "2" else False
+import hmac
+import time
+import toml
 
 def app():
-    st.title('Login to Recycle AI')
-    input_username = st.text_input("Username", key='input_username')
-    input_password = st.text_input("Password", type="password", key='input_password')
+    st.header("Register for Recycle AI")
+    st.write("Create an account to use Recycle AI.")
 
-    if st.button('Login'):
-        if check_credentials(input_username, input_password):
-            st.session_state['logged_in'] = True
-            st.session_state['username'] = input_username 
-            st.success(f"Welcome {input_username}!")
-            st.experimental_rerun()
-        else:
-            st.error("Incorrect username or password")
+
+def check_password():
+  """Returns `True` if the user had a correct password."""
+  
+
+  def login_form():
+      """Form with widgets to collect user information"""
+      with st.form("Credentials"):
+          st.text_input("Username", key="username")
+          st.text_input("Password", type="password", key="password")
+          st.form_submit_button("Log in", on_click=password_entered)
+
+  def password_entered():
+      """Checks whether a password entered by the user is correct."""
+    
+      if st.session_state["username"] in st.secrets[
+          "passwords"
+      ] and hmac.compare_digest(
+          st.session_state["password"],
+          st.secrets.passwords[st.session_state["username"]],
+      ):
+          st.session_state["password_correct"] = True
+          del st.session_state["password"]  # Don't store the username or password.
+          del st.session_state["username"]
+      else:
+          st.session_state["password_correct"] = False
+
+  # Return True if the username + password is validated.
+  if st.session_state.get("password_correct", False):
+      return True
+
+  # Show inputs for username + password.
+  login_form()
+  if "password_correct" in st.session_state:
+      st.error("😕 User not known or password incorrect")
+  return False
+
+
+if not check_password():
+  st.stop()
 
 # TODO: by Aryan
 # Use https://docs.streamlit.io/library/api-reference for reference
